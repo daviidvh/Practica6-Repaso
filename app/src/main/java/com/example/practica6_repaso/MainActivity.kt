43bca3ab582069d6c177a29eb6ef29dc8e58aa45
daviidvh <davvel833@alu.centroafuera.es>
2023-01-15 15:04:27 +0100

    Por acabar

--- app/src/main/java/com/example/practica6_repaso/MainActivity.kt
+++ app/src/main/java/com/example/practica6_repaso/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.practica6_repaso
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var activo=false
+        var boton=findViewById<Button>(R.id.buttonComenzar)
+        var eD1=findViewById<EditText>(R.id.editText)
+
+
+        boton.setOnClickListener() {
+            eD1.setOnFocusChangeListener { x, foco ->
+                if(foco)
+                        boton.text="Finalizar"
+                else{
+                    boton.text="Comenzar"
+                    }
+            }
+        }
+/*
+        if (findViewById<EditText>(R.id.editText).text.toString().contentEquals("Wayne"))
+            findViewById<ImageView>(R.id.batman).isVisible=true
+*/
     }
 }
